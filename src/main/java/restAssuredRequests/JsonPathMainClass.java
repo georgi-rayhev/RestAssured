7de423d620a37af26c866a825cbb990460ec5632
Georgi Raychev <georgi.raychev@estafet.com>
2022-07-15 10:01:36 +0300

    Json path examples

---
+++ src/main/java/restAssuredRequests/JsonPathMainClass.java
@@ -0,0 +1,57 @@
+package restAssuredRequests;
+
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import java.util.List;
+import java.util.Map;
+
+public class JsonPathMainClass {
+    private static final String URL = "https://api.publicapis.org/";
+
+    private static void getResponseHeader(String value) {
+        System.out.println("Header with value : " + value + " is : ");
+        System.out.println(RestAssured
+                .given()
+                .when()
+                .get(URL)
+                .getHeader(value));
+    }
+
+    public static void main(String[] args) {
+
+        RestAssured.baseURI = URL;
+
+        Response entries = RestAssured.given().when().get("entries");
+        Response categories = RestAssured.given().when().get("categories");
+
+        JsonPath entriesJsonPath = entries.jsonPath();
+        JsonPath categoriesJsonPath = categories.jsonPath();
+
+        //get String
+        String category = entriesJsonPath.getString("entries[100].Category");
+        System.out.println("Get String: " + category);
+
+        //get Int
+
+        //get List
+        List<String> listOfCategories = categoriesJsonPath.getList("categories");
+        System.out.println("Categories are : " + listOfCategories);
+
+        //get Boolean
+//
+        Boolean checkForBoolean = entriesJsonPath.getBoolean("entries[55].HTTPS");
+        System.out.println("The boolean is : " + checkForBoolean);
+
+        //get Object
+        Object jsonObject =  entriesJsonPath.getJsonObject("entries[25]");
+        System.out.println("The Object is : " + jsonObject);
+
+        //get Map
+        Map<String, String> map = entriesJsonPath.getMap("entries[0]");
+
+        for (Map.Entry<String, String> objectObjectEntry : map.entrySet()) {
+            System.out.println(objectObjectEntry);
+        }
+    }
+}
